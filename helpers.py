9f1e4ad229e311b3d6979a170e3255eb0334e6b8
mdorster <manon.dorster@epfl.ch>
2022-11-24 18:15:02 +0100

    project start

---
+++ helpers.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+def calculate_mse(e):
+    """Calculate the mse for vector e."""
+    return 1 / 2 * np.mean(e**2)
+
+def compute_loss(y, tx, w):
+    e = y - tx.dot(w)
+    return calculate_mse(e)
+
+def compute_gradient(y, tx, w):
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
+
+def gradient_descent(y, tx, initial_w, max_iters, gamma):
+
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    prev_loss = 0
+    loss = 0
+    w = initial_w
+    
+    for n_iter in range(max_iters):
+        ### SOLUTION
+        # compute loss, gradient
+        grad, err = compute_gradient(y, tx, w)
+        prev_loss = loss
+        loss = calculate_mse(err)
+        
+        if loss < prev_loss:
+            w = w - gamma * grad
+
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        
+        print("Iteration:", n_iter)
+        print("Loss:", loss)
+        print("Prev loss:", prev_loss)
+
+    return w, loss
+
+def standardize(x):
+    #Standardize the original data set.
+    mean_x = np.mean(x, axis=0)
+    x = x - mean_x
+    std_x = np.std(x, axis=0)
+    x = x / std_x
+    return x
+
+def least_squares(y, tx):
+    #We solve the normal equations using QR decomposition, which is a computationally efficient method.
+    a = tx.T.dot(tx)
+    b = tx.T.dot(y)
+
+    w = np.linalg.solve(a,b)
+
+    err = y - tx.dot(w)
+    loss = np.mean(err**2)/2
+    
+    return w, loss
+
+def predict(tx, w):
+    """ 
+    Computes the age prediction according to the weight matrix.
+    The return value is rounded to the nearest integer. 
+    """
+    y = tx.dot(w)
+    return np.rint(y)
